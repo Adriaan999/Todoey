5877b4f633fd20aaf85d46ffd9adc337226dd165
Adriaan <adriaan9@hotmail.co.za>
2019-04-02 11:57:31 +0200

    Replaced core data with realm

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Adriaan on 2019/04/02.
+//  Copyright © 2019 Adriaan. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
